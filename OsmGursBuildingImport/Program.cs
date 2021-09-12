b6c3342a863c19b2e45a03337e48a1e3c1826154
David Karlaš <david.karlas@gmail.com>
2021-09-12 05:55:58 +0200

    Move TaskManager .json creation logic to new file

--- OsmGursBuildingImport/Program.cs
+++ OsmGursBuildingImport/Program.cs
@@ -1,96 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web;
-using NetTopologySuite.Features;
 using NetTopologySuite.Geometries;
 using NetTopologySuite.Index.Strtree;
-using NetTopologySuite.IO;
-using NetTopologySuite.Simplify;
-using Newtonsoft.Json;
 using OsmSharp;
 using OsmSharp.Complete;
 using OsmSharp.Db;
 using OsmSharp.Db.Impl;
 using OsmSharp.Geo;
 using OsmSharp.Streams;
-using OsmSharp.Tags;
 
 namespace OsmGursBuildingImport
 {
     partial class Program
     {
-        public class StmTask
-        {
-            [JsonProperty("name")]
-            public string Name { get; set; }
-
-            [JsonProperty("processPoints")]
-            public int ProcessPoints { get; set; }
-
-            [JsonProperty("maxProcessPoints")]
-            public int MaxProcessPoints { get; set; }
-
-            [JsonProperty("geometry")]
-            public string Geometry { get; set; }
-
-            [JsonProperty("assignedUser")]
-            public string AssignedUser { get; set; }
-        }
-
-        public class StmProject
-        {
-            [JsonProperty("name")]
-            public string Name { get; set; }
-
-            [JsonProperty("users")]
-            public List<string> Users { get; set; }
-
-            [JsonProperty("owner")]
-            public string Owner { get; set; }
-
-            [JsonProperty("description")]
-            public string Description { get; set; }
-
-            [JsonProperty("creationDate")]
-            public DateTime CreationDate { get; set; }
-
-            [JsonProperty("tasks")]
-            public List<StmTask> Tasks { get; set; }
-        }
-
-        public class JsonPolygon
-        {
-            [JsonProperty("file_name")]
-            public string FileName { get; set; }
-
-            [JsonProperty("file_type")]
-            public string FileType { get; set; }
-        }
-
-        public class Extract
-        {
-            [JsonProperty("output")]
-            public string Output { get; set; }
-
-            [JsonProperty("polygon")]
-            public JsonPolygon Polygon { get; set; }
-        }
-
-        public class Root
-        {
-            [JsonProperty("directory")]
-            public string Directory { get; set; }
-
-            [JsonProperty("extracts")]
-            public List<Extract> Extracts { get; set; }
-        }
-
-
         record GeoOsmWithGeometry(Geometry Geometry, ICompleteOsmGeo OsmGeo);
 
         static int Main(string[] args)
@@ -129,115 +54,7 @@ namespace OsmGursBuildingImport
             var osmIndexTask = Task.Run(() => LoadOsmData(pbfFile));
             var model = new GursData(tempDir, overridesDir);
             STRtree<GeoOsmWithGeometry> osmIndex = osmIndexTask.Result;
-
-            var baseUrlForBlobs = "https://osm-func.azurewebsites.net/api/BlobDownloader/";
-
-            var ljubljana = model.Settlements.Values.Single(s => s.Name.Name == "Ljubljana").Geometry;
-            var project = new StmProject()
-            {
-                Name = "Gurs Buildings&Addresses Import",
-                Owner = "12422736",
-                Users = new(new[] { "12422736" }),
-                Tasks = new List<StmTask>(),
-                CreationDate = DateTime.Now,
-                Description = "How to do this, go here: http://example.com"
-            };
-
-            var json = new Root();
-            json.Directory = "data/output/";
-            json.Extracts = new List<Extract>();
-
-            var geoJsonWriter = new GeoJsonWriter();
-            foreach (var area in model.ProcessingAreas)
-            {
-                var josmCommands = new List<string>{
-                    $"http://localhost:8111/import?new_layer=true&layer_locked=true&layer_name=All%20Buildings&url={HttpUtility.UrlEncode($"{baseUrlForBlobs}{area.Name}.full.osm")}",
-                    $"http://localhost:8111/import?new_layer=true&layer_name=Merge%20This&url={HttpUtility.UrlEncode($"{baseUrlForBlobs}{area.Name}.merge.osm")}",
-                    $"http://localhost:8111/import?new_layer=true&layer_name=OSM%20Data&url={HttpUtility.UrlEncode($"{baseUrlForBlobs}{area.Name}.original.osm")}"
-                };
-
-                josmCommands.Add($"http://localhost:8111/zoom?left={area.Geometry.EnvelopeInternal.MinX}&right={area.Geometry.EnvelopeInternal.MaxX}&top={area.Geometry.EnvelopeInternal.MaxY}&bottom={area.Geometry.EnvelopeInternal.MinY}");
-
-                if (ljubljana.Intersects(area.Geometry))
-                {
-                    josmCommands.Add("http://localhost:8111/imagery?id=LJUBLJANA-DOF-2020");
-                }
-                else
-                {
-                    josmCommands.Add("http://localhost:8111/imagery?id=GURS-DOF025");
-                }
-
-                josmCommands.Add("http://localhost:8111/imagery?id=GURS-buildings");
-
-                // Turns out relation between area of geometry and distanceTolerance is kind of perfect...
-                var simplePoly = TopologyPreservingSimplifier.Simplify(area.Geometry, area.Geometry.Area);
-
-                project.Tasks.Add(new StmTask()
-                {
-                    Geometry = geoJsonWriter.Write(new Feature(simplePoly,
-                         new AttributesTable(new Dictionary<string, object> {
-                            { "name", area.Name },
-                            { "josmCommands", josmCommands.ToArray()}}))),
-                    MaxProcessPoints = area.Buildings.Count,
-                    Name = area.Name
-                });
-
-                Polygon[] polygons;
-                if (area.Geometry is MultiPolygon mp)
-                {
-                    polygons = mp.Geometries.OfType<Polygon>().ToArray();
-                    if (polygons.Length != mp.Geometries.Length)
-                        throw new Exception(string.Join(", ", mp.Geometries.Select(g => g.GetType().ToString())));
-                }
-                else if (area.Geometry is Polygon poly2)
-                {
-                    polygons = new[] { poly2 };
-                }
-                else
-                {
-                    throw new Exception(area.Geometry.GetType().ToString());
-                }
-
-
-                string polyPath = Path.Combine(tempDir, "polygons", area.Name + ".poly");
-                using var sw = new StreamWriter(polyPath);
-                sw.WriteLine(area.Name + ".original");
-                for (int i = 0; i < polygons.Length; i++)
-                {
-                    sw.WriteLine("poly" + (i + 1));
-                    foreach (var cord in polygons[i].Coordinates)
-                    {
-                        sw.WriteLine($"\t{cord.X} {cord.Y}");
-                    }
-                    sw.WriteLine("END");
-                }
-                sw.WriteLine("END");
-
-                json.Extracts.Add(new Extract()
-                {
-                    Output = area.Name + ".original.osm",
-                    Polygon = new JsonPolygon()
-                    {
-                        FileType = "poly",
-                        FileName = Path.Combine("data", "temp", "polygons", area.Name + ".poly")
-                    }
-                });
-            }
-            File.WriteAllText(Path.Combine(outputFolder, "Tasks.json"), JsonConvert.SerializeObject(project));
-            int index = 0;
-            var osmiumFolder = Path.Combine(tempDir, "osmium");
-            Directory.CreateDirectory(osmiumFolder);
-            const int OSMIUM_EXPORTS_AT_ONCE = 50;
-            while (json.Extracts.Count > 0)
-            {
-                index++;
-                File.WriteAllText(Path.Combine(osmiumFolder, $"export{index}.json"), JsonConvert.SerializeObject(new Root()
-                {
-                    Directory = json.Directory,
-                    Extracts = new(json.Extracts.Take(OSMIUM_EXPORTS_AT_ONCE))
-                }));
-                json.Extracts.RemoveRange(0, Math.Min(json.Extracts.Count, OSMIUM_EXPORTS_AT_ONCE));
-            }
+            SimpleTaskManager.CreateStmProjectJson(outputFolder, tempDir, model);
 
             Parallel.ForEach(model.ProcessingAreas, (processingArea) =>
             {
@@ -294,10 +111,6 @@ namespace OsmGursBuildingImport
             var osmIndex = new STRtree<GeoOsmWithGeometry>();
             foreach (var osmGeo in osmStream)
             {
-                if (!osmGeo.Tags.ContainsKey("building"))
-                {
-                    continue;
-                }
                 var osmGeoComplete = osmGeo.CreateComplete(db);
                 var featureCollection = interpreter.Interpret(osmGeoComplete);
                 if (featureCollection.Count != 1)
